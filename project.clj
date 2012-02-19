c773e80cf81c79e33be10b74db265918071fa7d5
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2012-02-19 23:57:05 +0000

    jaxb optional

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scufl2-clojure "1.0.0-SNAPSHOT"
+(defproject scufl2 "1.0.0-SNAPSHOT"
   :description "Scufl2 API for Clojure"
   :dependencies [
                  [org.clojure/clojure "1.3.0"]
@@ -6,6 +6,10 @@
                  [uk.org.taverna.scufl2/scufl2-api "0.9"]
                  [uk.org.taverna.scufl2/scufl2-rdfxml "0.9"]
                  [uk.org.taverna.scufl2/scufl2-t2flow "0.9"]
-                 ]
+
+                 ;; optional
+                 [com.sun.xml.bind/jaxb-impl "2.2.4-1"]
+
+                ]
   :repositories {"mygrid-repository" "http://www.mygrid.org.uk/maven/repository/"}
 )
