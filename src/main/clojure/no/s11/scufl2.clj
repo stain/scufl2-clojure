8326aaf45d3913c0c5f4f70e63380282593f34e5
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2012-02-19 23:36:10 +0000

    almost working!

---
+++ src/main/clojure/no/s11/scufl2.clj
@@ -0,0 +1,27 @@
+(ns no.s11.scufl2
+  (:import (uk.org.taverna.scufl2.api.io WorkflowBundleIO) 
+           (uk.org.taverna.scufl2.api.common URITools Scufl2Tools)
+           (java.io File)
+  )
+)
+
+(println "Hello")
+
+(def io (new WorkflowBundleIO))
+(def uri (new URITools))
+(def tools (new Scufl2Tools))
+
+(def MIME-WFBUNDLE "application/vnd.taverna.scufl2.workflow-bundle")
+(def MIME-T2FLOW "application/vnd.taverna.t2flow+xml")
+
+(defn read-wfbundle 
+  "Read a Scufl2 workflow bundle from given filename"
+  [filename] 
+  (.readBundle io (new File filename) nil))
+
+(defn write-wfbundle 
+  "Write a Scufl2 workflow bundle to given filename"
+  [filename] 
+  (.writeBundle io (new File filename) "application/vnd.taverna.scufl2.workflow-bundle"))
+
+
---
+++ src/test/clojure/scufl2-test.clj
@@ -0,0 +1,10 @@
+(ns scufl2-test
+  ;(:require no.s11.scufl2)
+  (:use clojure.test)
+ ; (:use no.s11.scufl2)
+  )
+
+(deftest readbundle
+     (is (= "Hello_world" "Hello_World"))
+         
+)    
