df10fe7111c3a7fe177d5e909f9f0d725bc79821
Stian Soiland-Reyes <stian@soiland-reyes.com>
2012-10-02 17:36:03 +0100

    Use scufl2 0.9.2

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
-(defproject scufl2 "1.0.0-SNAPSHOT"
+(defproject scufl2 "1.0.1-SNAPSHOT"
   :description "Scufl2 API for Clojure"
   :dependencies [
-                 [org.clojure/clojure "1.3.0"]
-;                 [org.clojure/clojure-contrib "1.3.0"]
-                 [uk.org.taverna.scufl2/scufl2-api "0.9"]
-                 [uk.org.taverna.scufl2/scufl2-rdfxml "0.9"]
-                 [uk.org.taverna.scufl2/scufl2-t2flow "0.9"]
+                 [org.clojure/clojure "1.4.0"]
+;                 [org.clojure/clojure-contrib "1.4.0"]
+                 [uk.org.taverna.scufl2/scufl2-api "0.9.2"]
+                 [uk.org.taverna.scufl2/scufl2-rdfxml "0.9.2"]
+                 [uk.org.taverna.scufl2/scufl2-t2flow "0.9.2"]
 
+                 
                  ;; optional
                  [com.sun.xml.bind/jaxb-impl "2.2.4-1"]
 
