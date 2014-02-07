94032d0ce5d4554b62e0af3839427bf2dc17c217
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-02-07 16:12:45 +0000

    newer clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject scufl2 "1.0.1-SNAPSHOT"
   :description "Scufl2 API for Clojure"
   :dependencies [
-                 [org.clojure/clojure "1.4.0"]
-;                 [org.clojure/clojure-contrib "1.4.0"]
+                 [org.clojure/clojure "1.5.1"]
+;                 [org.clojure/clojure-contrib "1.5.1"]
                  [uk.org.taverna.scufl2/scufl2-api "0.13.1"]
                  [uk.org.taverna.scufl2/scufl2-rdfxml "0.13.1"]
                  [uk.org.taverna.scufl2/scufl2-t2flow "0.13.1"]
