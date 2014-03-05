b20b0d893ac4d02e155432cc109635bd413736fd
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2014-03-05 09:52:07 +0000

    tests

--- test/scufl2/test/core.clj
+++ test/scufl2/test/core.clj
@@ -8,7 +8,7 @@
 (defn helloworld []
   (read-bundle "test/helloworld.wfbundle"))
 
-(deftest bundle-from-file
+(deftest read-bundle-from-file
   (is (= "Hello_World" (.getName (helloworld)))))
 
 (defn tempfile []
@@ -16,14 +16,42 @@
     (.delete)
     (.deleteOnExit)))
 
-(deftest bundle-to-file
+(deftest test-write-bundle
   (let [tmp (tempfile)]   
     (is (not (.exists tmp)))
     (write-bundle (new uk.org.taverna.scufl2.api.container.WorkflowBundle) tmp)
     (is (.exists tmp))))
 
-(deftest check-structure
-  (let [structure (bundle-structure (helloworld))]
-    (is (= '(wfbundle) structure))))
+(deftest test-workflowbundle-to-clj
+  (let [wfb (workflowbundle-to-clj (helloworld))]
+    (is (= "Hello_World" (:name wfb)))
+    (is (= "http://ns.taverna.org.uk/2010/workflowBundle/8781d5f4-d0ba-48a8-a1d1-14281bd8a917/" (:identifier wfb)))
+    (is (= "Hello_World" (:mainWorkflow wfb)))
+    (is (= '("Hello_World") (keys (:workflows wfb))))
+    (is (= "taverna-2.4.0" (:mainProfile wfb)))
+    (is (= '("taverna-2.4.0") (keys (:profiles wfb))))
+    ))
+
+(deftest test-main-workflow
+  (let [wfb { :mainWorkflow "fred"
+              :workflows {"soup" :soupWf
+                          "fred" :fredWf
+                          }
+             }]
+      (is (= :fredWf (main-workflow wfb))))
+  (let [no-main { :workflows { "soup" :soup } } ]
+      (is (nil? (main-workflow no-main))))
+  (let [missing-main { :mainWorkflow "missing"
+                       :workflows {} }]
+      (is (nil? (main-workflow missing-main)))))
+
+      
+
+(deftest test-workflow-to-clj
+  (let [wf (workflow-to-clj (.getMainWorkflow (helloworld)))]
+      (is (= '() (:controlLinks wf))) 
+))
+
+
 
 
